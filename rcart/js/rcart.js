e8ed3099252a60d87ea39873cd12971f2b9a2e25
oshige <oshige@mono-96.jp>
2017-04-26 20:11:32 +0900

    Add multiple order for cart

--- rcart/js/rcart.js
+++ rcart/js/rcart.js
@@ -6,63 +6,71 @@ $(function() {
         return false;
     })
 });
-$("select").attr("disabled", "disabled");
+//$("select").attr("disabled", "disabled");
 
 //jquery ajax form.serialize
 
-$("#allDummy").on('change', function() {
-    //get all form value
-    var $form = $('#allform');
-    var query = $form.serialize();
-    console.log(query);
-    var param = $form.serializeArray();
-    console.log(param);
+// $("#allDummy").on('change', function() {
+//     //get all form value
+//     var $form = $('#allform');
+//     var query = $form.serialize();
+//     console.log(query);
+//     var param = $form.serializeArray();
+//     console.log(param);
 
-    console.log(query); // => "mytext=sa
-    //選択したvalue値を変数に格納
-    var val = $("#cart_122").val();
-    console.log(val);
-    //選択したvalue値をp要素に出力
-    $('#sel').text(val);
-    $('#result1').empty();
-    $('#result').empty();
-    $.ajax({
-        method: 'GET',
-        dataType: 'jsonp',
-        crossDomain: true,
-        url: 'https://direct.step.rakuten.co.jp/rms/mall/cartAdd/?shopid=321072&itemid=10000051&units=1&device=pc&userid=itempage',
-        success: function(result) {
-            alltext = result; //copy
-            alltext = JSON.stringify(result); //to string
-            $('#result').text(alltext);
-            //       json = JSON.parse(result);//divide
-            json = $.parseJSON(alltext);
-            $('#result1').text(json.dialogTitle + json.resultMessage);
-        }
-    });
-});
+//     console.log(query); // => "mytext=sa
+//     //選択したvalue値を変数に格納
+//     var val = $("#cart_122").val();
+//     console.log(val);
+//     //選択したvalue値をp要素に出力
+//     $('#sel').text(val);
+//     $('#result1').empty();
+//     $('#result').empty();
+//     $.ajax({
+//         method: 'GET',
+//         dataType: 'jsonp',
+//         crossDomain: true,
+//         url: 'https://direct.step.rakuten.co.jp/rms/mall/cartAdd/?shopid=321072&itemid=10000051&units=1&device=pc&userid=itempage',
+//         success: function(result) {
+//             alltext = result; //copy
+//             alltext = JSON.stringify(result); //to string
+//             $('#result').text(alltext);
+//             //       json = JSON.parse(result);//divide
+//             json = $.parseJSON(alltext);
+//             $('#result1').text(json.dialogTitle + json.resultMessage);
+//         }
+//     });
+// });
 
 /* shopjapan
 shop_bid=199614
 trce-s
 https://direct.step.rakuten.co.jp/rms/mall/cartAdd/?shopid=199614&itemid=10000070&units=1&device=pc&userid=itempage
+cero
+s  10000729
+sd 10000730
+d  10000731
 
 */
-function addCart() {
-    $('#result1').empty();
-    $('#result').empty();
+function addCart(itemid,units) {
+    shop_bid='199614'; // shopjapan id
+
     $.ajax({
         method: 'GET',
         dataType: 'jsonp',
         crossDomain: true,
-        url: 'https://direct.step.rakuten.co.jp/rms/mall/cartAdd/?shopid=321072&itemid=10000051&units=1&device=pc&userid=itempage',
+        url: 'https://direct.step.rakuten.co.jp/rms/mall/cartAdd/?shopid='+shop_bid+'&itemid='+itemid+'&units='+units+'&device=pc&userid=itempage',
         success: function(result) {
-            alltext = result; //copy
             alltext = JSON.stringify(result); //to string
-            $('#result').text(alltext);
-            //       json = JSON.parse(result);//divide
+            $('#result').append(alltext);
             json = $.parseJSON(alltext);
-            $('#result1').text(json.dialogTitle + json.resultMessage);
+            console.log(json);
+            $('#result1').append(json.dialogTitle + json.resultMessage);
+        },
+        error: function(result){
+            alltext = JSON.stringify(result); //to string
+            console.log(alltext);
+            // return result;
         }
     });
 }
@@ -78,7 +86,7 @@ function addCartkuroko() {
         success: function(result) {
             alltext = result; //copy
             alltext = JSON.stringify(result); //to string
-            $('#result').text(alltext);
+            $('#result1').text(alltext);
             json = $.parseJSON(alltext);
             $('#result1').text(json.dialogTitle + json.resultMessage);
         }
@@ -88,37 +96,48 @@ function addCartkuroko() {
 
 // <script>
 //全フィールドを走査して表示。
-function showValues() {
+function showValues(on_off) {
+    // var goAddCart = goAddCart || false;
     var fields = $(":input").serializeArray();
-    var select_flg = "";
-    $("#result1").empty();
+    // new on_off = on_off;
+    // fields.push(['on_off:'+on_off ]);
+    // $("#result1").empty();
     $('.ui.sidebar').sidebar('toggle');
-
+    flgArray = new Array();
     jQuery.each(fields, function(i, field) {
+        var radioFlg ="";  //mat
+        var selectFlg =""; //option
         if (field.value == "数量") {
             return true; // continue の代わりskipする
         }
         // radio 前方一致のときの処理
-        if (!field.name.indexOf('radio') && field.value !== "") {
+        if (!field.name.indexOf('radio') && field.value !== "" ) {
+            flgArray['radioFlg']='on';
             $("#result1").append(field.name + " ");
-            $("#reset_radio").removeClass("disabled");
-            $("select").removeAttr("disabled"); //選び直すボタン ON
-            var select_flg = "on";
+            // addCart(itemid,units);
+            if(on_off==="on"){
+                addCart(field.value,units=1);
+            }
         }
         // select option > cart 前方一致のときの処理
-        if (!field.name.indexOf('cart') && field.value !== "") {
-          $("#go_cart").removeClass("disabled");
-          $("#result1").append(field.name + " ");
+        if (!field.name.indexOf('1000') && field.value !== "" ) {
+            flgArray['selectFlg']="on";
+            $("#result1").append(field.name + " ");
+            if(on_off==="on"){
+                addCart(field.name,field.value);
+            }
         }
         $("#result1").append(field.name + " ");
         $("#result1").append(field.value + " ");
         $("#results_all").append(field.name + " ");
         $("#results_all").append(field.value + " ");
     });
-    if (select_flg == "on") {
-        // $("select").removeAttr("disabled");
-    } else {
-        // $("select").attr("disabled", "disabled");
+    if (flgArray['radioFlg']==="on" ) {//Each外部に出せない！
+        $("select").removeAttr("disabled"); //Select hyouji
+        $("#reset_radio").removeClass("disabled");//選び直すボタン 表示
+    }
+    if(flgArray['radioFlg']==="on" && flgArray['selectFlg']==="on") {
+        $("#go_cart").removeClass("disabled");//購入ボタン 表示
     }
 
 }
@@ -131,6 +150,8 @@ function reset_radio() {
         $("input[type='radio']").attr("checked", false);
         $("#reset_radio").attr("disabled", false);
         $("select").attr("disabled", false);
+        // $("#go_cart").attr("disabled", false);
+          $("#go_cart").addClass("disabled");
     } else {
         // キャンセルならダイアログ表示
     }
@@ -140,7 +161,15 @@ $(":checkbox, :radio").click(showValues);
 $("select").change(showValues);
 $("#reset_radio").click(reset_radio);
 $("#addCartkuroko").click(addCartkuroko);
-$("#addCart").click(addCart);
 
-showValues();
+// one item add cart
+// $("#addCartSJ").click(addCart(itemid,units));
+$("#addCartSJ").click(showValues('on'));
+
+// all seek add cart
+$("#go_cart").click(showValues('on'));
+
+// showValues();
+
+
 // </script>
