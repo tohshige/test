2e1f01c3f3cd6121c83c280ef48c4c5ac1f832aa
oshige <oshige@mono-96.jp>
2017-05-01 14:58:07 +0900

    Fix new modal

--- rcart/js/rcart.js
+++ rcart/js/rcart.js
@@ -1,5 +1,20 @@
 // Shift-JIS
 
+// $(function(){
+//   $('#btnBuy').click(function(){
+//     var speed = 500;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? 'html' : href);
+//     var position = target.offset().top;
+//     $("html, body").animate({scrollTop:position}, speed, "swing");
+//     return false;
+//   });
+// });
+$("a[href='#btnBuy']").click(function() {
+  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  return false;
+});
+
 // href –³Œø
 $(function () {
     $('a.disable').click(function () {
