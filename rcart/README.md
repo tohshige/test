3b8def54f028dcc1432b5fe21eb54feefb859953
oshige <oshige@mono-96.jp>
2017-05-01 18:13:34 +0900

    Description for radio & dropdown.

--- rcart/README.md
+++ rcart/README.md
@@ -5,4 +5,8 @@ http://mono-96.jp/tmp/test/rcart/cart_test.html
 http://mono-96.jp/tmp/test/rcart/
 
 
+例：画面上部、ラジオボタン例
+https://github.com/tohshige/test/blob/master/rcart/index.html#L141,L161
 
+例：画面下の商品ドロップダウン
+https://github.com/tohshige/test/blob/master/rcart/index.html#L523,L543
--- rcart/js/rcart.js
+++ rcart/js/rcart.js
@@ -1,15 +1,6 @@
 // Shift-JIS
 
-// $(function(){
-//   $('#btnBuy').click(function(){
-//     var speed = 500;
-//     var href= $(this).attr("href");
-//     var target = $(href == "#" || href == "" ? 'html' : href);
-//     var position = target.offset().top;
-//     $("html, body").animate({scrollTop:position}, speed, "swing");
-//     return false;
-//   });
-// });
+// SmoothScroll
 $("a[href='#btnBuy']").click(function() {
   $("html, body").animate({ scrollTop: $(document).height() }, "slow");
   return false;
