525196b79244ec02473d53f8b02ead091a70884d
User Mono-96 <mono-96@www1093.sakura.ne.jp>
2017-10-18 15:23:58 +0900

    add designed by Ksuke

---
+++ rcart2/js/chnageimg.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+
+
+$(function(){
+    $('#thumb_area img').click(function(){
+        $('#main_img img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area2 img').click(function(){
+        $('#main_img2 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area3 img').click(function(){
+        $('#main_img3 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area4 img').click(function(){
+        $('#main_img4 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area5 img').click(function(){
+        $('#main_img5 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area6 img').click(function(){
+        $('#main_img6 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area7 img').click(function(){
+        $('#main_img7 img').attr('src', $(this).attr('src'));
+    });
+});
+
+
+$(function(){
+    $('#thumb_area8 img').click(function(){
+        $('#main_img8 img').attr('src', $(this).attr('src'));
+    });
+});
