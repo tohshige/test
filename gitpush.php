ff9586ba22dbcba4b2650af2e7beb707ddeee570
User Mono-96 <mono-96@www1093.sakura.ne.jp>
2017-10-18 17:21:54 +0900

    auto git Commit and push at 2017-10-18 17:21:54

--- gitpush.php
+++ gitpush.php
@@ -1,8 +1,9 @@
 <?
+// auto git push .gitpush.php
 exec('cd /home/mono-96/www/mono-96.jp/tmp/test', $op, $rv);
 exec('/usr/local/bin/git status', $op, $rv);
 exec('/usr/local/bin/git add -A', $op, $rv);
-exec('/usr/local/bin/git commit -m "Commit at $(date "+%Y-%m-%d %T")" || true', $op, $rv);
+exec('/usr/local/bin/git commit -m "auto git Commit and push at $(date "+%Y-%m-%d %T")" || true', $op, $rv);
 exec('/usr/local/bin/git push -f origin master:master', $op, $rv);
 print_r($op);
 print_r($rv);
