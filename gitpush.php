803f3fe60f15768c6fa9ac9664f8c2118a3cef6a
oshige <oshige@mono-96.jp>
2018-01-19 10:23:37 +0900

    test add and delete line number

--- gitpush.php
+++ gitpush.php
@@ -7,5 +7,14 @@ exec('/usr/local/bin/git commit -m ":up:変更感知 $(date "+%m-%d %T")" || tru
 exec('/usr/local/bin/git push -f origin master:master', $op, $rv);
 print_r($op);
 print_r($rv);
-?>
 
+/* test add delete line number
+git log --numstat --pretty="%H" --since=2017-01-01 --until=2017-03-31 --no-merges \
+| awk 'NF==3 {plus+=$1; minus+=$2} END {printf("合計%d行 (追加+%d, 削除-%d)\n", plus+minus, plus, minus)}'
+
+git log --numstat --pretty="%H" --no-merges | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("%d (+%d, -%d)\n", plus+minus, plus, minus)}'
+git log --numstat --pretty="%H" | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("%d (+%d, -%d)\n", plus+minus, plus, minus)}'
+
+
+*/
+?>
