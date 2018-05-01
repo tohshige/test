94358f3e8033a6b9dcce2df7955c56a063114da2
oshige <oshige@mono-96.jp>
2018-05-01 11:40:42 +0900

    passed local test

--- deploy.php
+++ deploy.php
@@ -7,8 +7,10 @@ print_r($rv);
 // そんな時は、linux コマンドを使うべき。
 
 // $dir 取得したいフォルダパス
-$dir "/home/mono-96/www/mono-96.jp/tmp/test";
+$dir "/home/mono-96/www/mono-96.jp/tmp/test/rcart2/";
+// $dir ="/Users/mono05/Documents/test/";
 // $zipFileSavePath 一時、zipを保存しておくフォルダパス
+// $zipFileSavePath ="/Users/mono05/Documents/test/";
 $zipFileSavePath "/home/mono-96/www/mono-96.jp/tmp/";
 
 function getZip($dir,$zipFileSavePath){
@@ -36,5 +38,8 @@ function getZip($dir,$zipFileSavePath){
 // //    消す
 //     unlink($zipFileSavePath.$fileName.".zip");
 }
+
+// run
+getZip($dir, $zipFileSavePath);
 
 ?>
